20c753ba1cd7a7aca14c92bb6f1e2d9a22e4a2a5
Jialue Huang <jialuehuang@gmail.com>
2013-05-31 17:33:15 -0400

    Change proj1phase3_studentcode_new to proj1phase3_studentcode.

--- proj1phase3_studentcode/proj1phase3_studentcode_new/proj1phase3_studentcode_new/init_script.m
+++
@@ -1,6 +0,0 @@
-% Add addicitonal initialization if you want to.
-% You can use this space to pre-compute the trajectory to avoid repeatly compute the same trajectory in every call of the "trajectory_generator" function
-
-% Generate trajectory
-disp('Generating Trajectory ...');
-trajectory_generator([], map, path);
